c0423252f3ea090821ecb642f8e1838a741b40d3
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-04-26 13:04:19 +0200

    Complete ship implementation w/ tests

--- src/__test__/logic/ship.test.js
+++ src/__test__/logic/ship.test.js
@@ -1,5 +1,68 @@
 import ship from '../../logic/ship.js';
 
-test('returns an object', () => {
-  expect(typeof ship()).toBe('object');
+describe('with arguments', () => {
+  test('when no size is specified, returns default', () => {
+    expect(ship()).toMatchObject({ size: 2 });
+  });
+
+  test("doesn't accept negative values as size", () => {
+    expect(() => {
+      ship(-1);
+    }).toThrow();
+  });
+
+  test('sets ship squares with an array as 2nd argument', () => {
+    expect(ship(2, [true, true])).toMatchObject({ squares: [true, true] });
+  });
+
+  test('ignore optional array if its length differs from size', () => {
+    expect(ship(2, [true])).toMatchObject({ squares: [false, false] });
+  });
+
+  test('if first argument is a valid ship, returns copy', () => {
+    let newShip = ship(4, [false, true, true, false]);
+    expect(ship(newShip)).toMatchObject({
+      size: 4,
+      squares: [false, true, true, false],
+    });
+  });
+
+  test('throws if first argument is invalid ship', () => {
+    let fakeShip = { size: -1, squares: [true, false, true] };
+
+    expect(() => {
+      ship(fakeShip);
+    }).toThrow();
+  });
+});
+
+describe('return value', () => {
+  test('is an object', () => {
+    expect(typeof ship()).toBe('object');
+  });
+
+  test('has array of squares', () => {
+    expect(ship(2)).toMatchObject({ squares: [false, false] });
+  });
+
+  test('has a size', () => {
+    expect(ship(2)).toMatchObject({ size: 2 });
+  });
+});
+
+describe('functionality', () => {
+  test('records hits', () => {
+    let newShip = ship(2);
+    newShip.hit(1);
+    expect(newShip).toMatchObject({ squares: [true, false] });
+  });
+  test('tells you if it sunk', () => {
+    let sunkenShip = ship(2);
+    sunkenShip.hit(1);
+    sunkenShip.hit(2);
+    expect(sunkenShip.isSunk()).toBe(true);
+
+    let normalShip = ship(2);
+    expect(normalShip.isSunk()).toBe(false);
+  });
 });
--- src/logic/ship.js
+++ src/logic/ship.js
@@ -1,5 +1,31 @@
-function ship() {
-  return {};
+function ship(sizeOrObject = 2, squares) {
+  if (sizeOrObject?.size && sizeOrObject?.squares) {
+    let { size, squares } = sizeOrObject;
+
+    if (size > 0 && squares?.length === size) {
+      return ship(size, squares);
+    } else {
+      throw new Error('Invalid ship passed as first argument');
+    }
+  } else if (sizeOrObject < 0) throw new Error('Invalid size for a ship.');
+
+  let newSquares =
+    squares?.length === sizeOrObject
+      ? squares
+      : Array(sizeOrObject).fill(false);
+
+  return {
+    size: sizeOrObject,
+    squares: newSquares,
+    hit(where) {
+      if (where < 0 || where > this.size) return;
+
+      this.squares[where - 1] = true;
+    },
+    isSunk() {
+      return this.squares.every((square) => square === true);
+    },
+  };
 }
 
 export default ship;
