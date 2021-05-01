31d7d731469185dec78b4f2be9d0e62577fd6f54
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-05-01 14:48:49 +0200

    Remove cycleThrough method for 2D arrays

--- src/logic/util/array.js
+++ src/logic/util/array.js
@@ -3,19 +3,6 @@ function create2D(x, y, callback) {
     return Array.from({ length: x }, callback);
   });
 
-  new2DArray.cycleThrough = (callback) => {
-    let boundaryX, boundaryY;
-
-    boundaryY = new2DArray.length;
-    boundaryX = new2DArray[0].length;
-
-    for (let y = 0; y < boundaryY; ++y) {
-      for (let x = 0; x < boundaryX; ++x) {
-        new2DArray[y][x] = callback(new2DArray[y][x]);
-      }
-    }
-  };
-
   return new2DArray;
 }
 
